291ef7ec03ce1de714ca4c4452951eac3522b6b7
RamyGabra <47239608+RamyGabra@users.noreply.github.com>
2019-02-27 12:32:49 +0200

    Delete keys_dev.js

--- keys_dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+serv://ramyGabra:Nike-1234@angrynerds-shard-00-00-ymdpc.mongodb.net:27017,angrynerds-shard-00-01-ymdpc.mongodb.net:27017,angrynerds-shard-00-02-ymdpc.mongodb.net:27017/test?ssl=true&replicaSet=AngryNerds-shard-0&authSource=admin&retryWrites=true'
-}
