1b02e5ebf8ad6b31bce03e871b6ef540887b7760
DanielAshrafHanna <danielhanna0001@gmail.com>
2019-03-01 23:58:24 +0200

    notification

--- models/Notifications.js
+++ models/Notifications.js
@@ -1,5 +1,29 @@
-class Notifications{
-    constructor(){
-        
+const mongoose = require ('mongoose')
+
+
+var notificationSchema = new mongoose.Schema({
+    reciver: {
+        type: String
+
+    },
+    text: {
+        type: String
+    },
+    time:{
+        type: Date
+    },
+    importance:{
+        type: Number
+    },
+    case:{
+        type: Number
     }
+        
 }
+
+)
+
+// mongoose.model('notification',notificationSchema );   // we call notificationSchema when we want to inseet data
+                                                      // notification is the collection
+
+ module.exports = Notification = mongoose.model('Notifications', notificationSchema)       //// TUT5
