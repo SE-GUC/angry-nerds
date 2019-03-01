1fbf96615f7ac116c3706b9a069e50781ed3f910
SaraHatem <sarahatem187@gmail.com>
2019-03-01 03:28:05 +0200

    Board of Directors

--- models/BoardOfDirectors.js
+++ models/BoardOfDirectors.js
@@ -1,5 +1,56 @@
-class BoardOfDirectors{
-    constructor(){
-        
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create the schema
+const BoardSchema = new Schema({
+    companyID: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String, 
+        required: true
+    },
+    nationality: {
+        type: String, 
+        required: true
+    },
+    gender:{
+        type: String, 
+        required: true
+    },
+    birthdate:{
+        type: Number, 
+        required: true
+    },
+    ssid:{
+        type: Number, 
+        required: true
+    },
+    idType:{
+        type: String, 
+        required: true
+    },
+    investorType:{
+        type: String, 
+        required: true
+    },
+    address:{
+        type: String, 
+        required: true
+    },
+    position:{
+        type: String, 
+        required: true
     }
-}
+})
+
+module.exports = Board = mongoose.model('BoardOfDirectors', BoardSchema)
---
+++ validations/boardValidations.js
@@ -0,0 +1,49 @@
+const Joi = require('joi')
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            //companyID: Joi.string().min(3).max(500).required(),
+            firstName: Joi.string().min(3).max(100).required(),
+            middleName: Joi.string().min(3).max(100).required(),
+            lastName: Joi.string().min(3).max(500).required(),
+            nationality: Joi.string().required(),
+            gender: Joi.string().min(4).max(6).required(),
+            birthdate: Joi.number().min(3).max(500).required(),
+            ssid: Joi.number().max(14).required(),
+            idType: Joi.string().min(3).max(500).required(),
+            investorType: Joi.string(),
+            address:Joi.string().required(),
+            position:string().required()
+        }
+
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+           //companyID: Joi.string().min(3).max(500).required(),
+           firstName: Joi.string().min(3).max(100).required(),
+           middleName: Joi.string().min(3).max(100).required(),
+           lastName: Joi.string().min(3).max(500).required(),
+           nationality: Joi.string().required(),
+           gender: Joi.string().min(4).max(6).required(),
+           birthdate: Joi.number().min(3).max(500).required(),
+           ssid: Joi.number().max(14).required(),
+           idType: Joi.string().min(3).max(500).required(),
+           investorType: Joi.string(),
+           address:Joi.string().required(),
+           position:string().required()
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+}
+
+
+
+
+
+
+
+
