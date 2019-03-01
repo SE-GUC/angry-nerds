abd3f5590336fcacc76d611a8fe3632a73e09b50
RamyGabra <47239608+RamyGabra@users.noreply.github.com>
2019-03-01 21:29:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Investors = require('./routes/api/Investor')
+
+
+const app = express()
+
+const db = require('./config/keys').mongoURI
+
+mongoose
+    .connect(db)
+    .then(()=>console.log('connected to MongoDB'))
+    .catch(err => console.log(err))
+
+app.use(express.json())
+
+app.get('/', (req,res) => res.send(`<h1>GAFI WEBSITE!!!</h1>`))
+app.get('/Ramy', (req,res) => res.send(`<h1>Ramy test page</h1>`))
+
+app.use('/api/Investor', Investors)
+
+app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server on ${port}`))
+
