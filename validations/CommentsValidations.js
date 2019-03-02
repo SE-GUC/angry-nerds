2e165b34fbb1f19b593cff7dc11bc313a64943fb
RamyGabra <47239608+RamyGabra@users.noreply.github.com>
2019-03-02 21:53:05 +0200

    Add files via upload

---
+++ validations/CommentsValidations.js
@@ -0,0 +1,33 @@
+const Joi = require('joi')
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            // Comments_ID: Joi.required(),
+            text: Joi.required(),
+            date: Joi.required(),
+            Case: Joi.required(),
+            Reviewer: Joi.required(),
+            Lawyer: Joi.required(),
+            Investor: Joi.required()
+        }
+
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+            // Comments_ID: Joi.required(),
+            
+            text: Joi.required(),
+            date: Joi.required(),
+            Case: Joi.required(),
+            Reviewer: Joi.required(),
+            Lawyer: Joi.required(),
+            Investor: Joi.required()
+           
+        }
+
+        return Joi.validate(request, updateSchema)
+    }
+}
