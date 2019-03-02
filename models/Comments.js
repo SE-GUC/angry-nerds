8de42dfdebecfc701d53829f8e01a1362889e174
RamyGabra <47239608+RamyGabra@users.noreply.github.com>
2019-03-02 21:44:01 +0200

    Add files via upload

--- models/Comments.js
+++ models/Comments.js
@@ -1,6 +1,47 @@
-class Comments{
-    constructor(){
+const mongoose = require('mongoose')
+        const Schema = mongoose.Schema
+        
+        
+        const CommentsSchema = new Schema({
+            // Comments_ID: {
+            //     type: String
+            //     // required: true
+            // },
+            text: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: String,
+                required: true
+            },
+            Case: {
+                // type: String,
+                type: Schema.Types.ObjectId,
+                ref: 'Cases',
+                required: true
+            },
+            Reviewer: {
+                // type: String,
+                type: Schema.Types.ObjectId, 
+                ref: 'Staff',
+                required: true
+            },
+            Lawyer: {
+                // type: String ,
+                type: Schema.Types.ObjectId, 
+                ref: 'Staff',
+                required: true
+            },
+            Investor: {
+                // type: String,
+                type: Schema.Types.ObjectId, 
+                ref: 'Investor',
+                required: true
+            }
 
-    }
 
-}
+
+        })
+        
+        module.exports = Commentj = mongoose.model('Comments', CommentsSchema)
