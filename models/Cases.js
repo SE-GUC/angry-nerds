8ce98527cfbd81b65dc252db5e11479547283938
MalakOssama <47049423+MalakOssama@users.noreply.github.com>
2019-03-02 22:21:59 +0200

    Add files via upload

--- models/Cases.js
+++ models/Cases.js
@@ -1,5 +1,46 @@
-class Cases{
-    constructor(){
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+// Create the schema
+const CaseSchema = new Schema({
+    
+    caseStatus: {
+        type: String,
+        required: true
+    },
+    companyName: {
+        type: String,
+        required: true
+    },
+    companyType: {
+        type: String,
+        required: true
+    },
+    caseOpenSince: {
+        type: Date,
+        required: true
+    },
+    reviewerID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Staff',
+        required: true
+    },
+    lawyerID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Staff',                   
+        required: true
+    },
+    investorID: {
+    	type: mongoose.Schema.Types.ObjectId,
+        ref: 'Staff',
+        required: true
+    },
+    formID: {
+    	type: mongoose.Schema.Types.ObjectId,
+        ref: 'Forms',
+        required: true
     }
-}
+
+})
+
+module.exports = Cases = mongoose.model('Cases', CaseSchema)
