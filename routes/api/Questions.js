10c14a6d1d200fbf64b4916a063a0e69988bb685
fadyHemaya <fady.hemaya@student.guc.edu.eg>
2019-03-21 16:42:57 +0200

    UnregisteredViewQuestions to Dev

--- routes/api/Questions.js
+++ routes/api/Questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 const mongoose = require('mongoose')
-
+const projection = { _id: 0,question :1,answer :1,time :1}
 
 
 const Questions= require('../../models/Questions');
@@ -10,6 +10,15 @@ const validator= require('../../validations/QuestionsValidations')
 router.get('/', async (req,res) => {
     const ques = await Questions.find()
     res.json({data: ques})
+})
+
+router.get('/UnregisteredViewQuestions', async (req,res,next) => {
+    try{
+        const ques = await Questions.find({},projection)
+        res.json({data: ques})
+        }catch (err){
+            return next(err)
+        }
 })
 
 router.get('/:id', async (req,res) => {
