f802daaaf8160ca25d760ac069bd4014baf51c1b
DanielAshrafHanna <danielhanna0001@gmail.com>
2019-03-21 15:36:47 +0200

    testing

--- routes/api/BoardOfDirectors.js
+++ routes/api/BoardOfDirectors.js
@@ -1,15 +1,72 @@
-const express = require('express')
+ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+// var express = require('express');         ////////////
+// var app = express();                      ///////////
+// var bodyParser = require('body-parser');  ////////////
 
-const Board = require('../../models/BoardOfDirectors')
-const validator = require('../../Validations/boardValidations')
+
+
+// const Board = require('../../models/BoardOfDirectors')
+// const validator = require('../../Validations/boardValidations')
 
 router.get('/', async (req,res) => {   //get all board's directors
     const directors = await Board.find()
     res.json({data: directors})
 })
+// ////////////////////////TRESTING ////////////////////////
 
+// //configure body-parser for express
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+
+
+// //allow express to access our html (index.html) file
+// app.get('/index.html', function(req, res) {
+//         res.sendFile(__dirname + "/" + "index.html");
+//     });
+
+// //route the GET request to the specified path, "/user". 
+// //This sends the user information to the path  
+// app.post('/user', function(req, res){
+//         response = {
+//             companyID : req.body.companyID,
+//             firstName : req.body.firstName,
+//             middleName: req.body.middleName,
+//             nationality :  req.body.nationality,
+//             gender :  req.body.gender,
+//             birthdate :  req.body.birthdate,
+//             ssid :  req.body.ssid,
+//             idType :  req.body.idType,
+//             investorType :  req.body.investorType,
+//             address :  req.body.address,
+//             position :  req.body.position
+
+
+//             };
+        
+//         //this line is optional and will print the response on the command prompt
+//         //It's useful so that we know what infomration is being transferred 
+//         //using the server
+//         console.log(response);
+        
+//         //convert the response in JSON format
+//         res.end(JSON.stringify(response));
+//     });
+
+// //This piece of code creates the server  
+// //and listens to the request at port 8888
+// //we are also generating a message once the 
+// //server is created
+// var server = app.listen(8888, function(){
+//         var host = server.address().address;
+//         var port = server.address().port;
+//         console.log("Example app listening at http://%s:%s", host, port);
+//     });
+
+
+
+// /////////////////////   end of testing//////////////////////////
 
 //get a specific director according to his id
 router.get('/:id', async (req,res) => {
