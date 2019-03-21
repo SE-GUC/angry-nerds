6c266cb3cedd2f817b8f459c04eba93d3bc57b45
RamyGabra <RamyGabra5@gmail.com>
2019-03-21 15:19:45 +0200

    updated schema

--- validations/caseValidations.js
+++ validations/caseValidations.js
@@ -4,13 +4,32 @@ module.exports = {
     createValidation: request => {
         const createSchema = {
             caseStatus: Joi.string().min(3).max(500).required(),
-            companyName: Joi.string().min(3).max(500).required(),
-            companyType: Joi.string().min(3).max(500).required(),
+            
             caseOpenSince: Joi.date().required(),
+            caseClosedDate: Joi.date().required(),
+
             reviewerID: Joi.required(),
             lawyerID: Joi.required(),
             investorID: Joi.required(),
-            formID: Joi.required()
+            
+            //forms
+            form_type:Joi.string().min(3),
+            regulated_law: Joi.string().min(3).max(500),
+            legal_form: Joi.string().min(3).max(100),
+            arabic_name: Joi.string().min(3).max(100),
+            english_name: Joi.string().min(3).max(100),
+            government: Joi.string().min(3).max(100),
+            city: Joi.string().min(3).max(100),
+            hq_address: Joi.string().min(3).max(100),
+            hq_city: Joi.string().min(3).max(100),
+            main_center_phone: Joi.number(),
+            main_center_fax: Joi.string(),
+            currency: Joi.string().min(3).max(100),
+            equality_capital: Joi.number().min(50000),
+            managers: Joi.array()
+
+
+
         }
 
         return Joi.validate(request, createSchema)
@@ -19,13 +38,29 @@ module.exports = {
     updateValidation: request => {
         const updateSchema = {
             caseStatus: Joi.string().min(3).max(500).required(),
-            companyName: Joi.string().min(3).max(500).required(),
-            companyType: Joi.string().min(3).max(500).required(),
-            caseOpenSince: Joi.date().required(), 
+            
+            caseOpenSince: Joi.date().required(),
+            caseClosedDate: Joi.date().required(),
+
             reviewerID: Joi.required(),
             lawyerID: Joi.required(),
             investorID: Joi.required(),
-            formID: Joi.required()
+            
+            //forms
+            form_type:Joi.string().min(3),
+            regulated_law: Joi.string().min(3).max(500),
+            legal_form: Joi.string().min(3).max(100),
+            arabic_name: Joi.string().min(3).max(100),
+            english_name: Joi.string().min(3).max(100),
+            government: Joi.string().min(3).max(100),
+            city: Joi.string().min(3).max(100),
+            hq_address: Joi.string().min(3).max(100),
+            hq_city: Joi.string().min(3).max(100),
+            main_center_phone: Joi.number(),
+            main_center_fax: Joi.string(),
+            currency: Joi.string().min(3).max(100),
+            equality_capital: Joi.number().min(50000),
+            managers: Joi.array()
         }
 
         return Joi.validate(request, updateSchema)
