672084db027c43823190802779ced7676b026726
RamyGabra <RamyGabra5@gmail.com>
2019-03-26 19:11:11 +0200

    hi

--- routes/api/Investor.js
+++ routes/api/Investor.js
@@ -47,17 +47,14 @@ router.post('/register', async (req, res) => {
     const user = await Investor.findOne({ email })
     if (user)
         return res.status(400).json({ error: 'Email already exists' })
-
-
-    // const isValidated = validator.createValidation(req.body)
-    // if(isValidated.error)
-    //     return res.status(400).send({ error: isValidated.error.details[0].message })
-
-    const newInvestor = await Investor.create(req.body)
-    res.json({ msg: 'Investor was created successfully', data: newInvestor })
+    else{
+        const newInvestor = await Investor.create(req.body)
+        res.json({ msg: 'Investor was created successfully', data: newInvestor })
         .catch(err => res.json('You could not be registered, try again'))
-
-})
+    }
+    
+    
+})  
 
 router.put('/:id', async (req, res) => {
     try {
