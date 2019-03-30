ef809a95f478ad9ebb757a82da34f70f51f40357
fady-wasfalla <fady.wasfalla@gmail.com>
2019-03-30 14:49:21 +0200

    merging fady medhat

--- app/Controllers/AdminController.js
+++ app/Controllers/AdminController.js
@@ -2,12 +2,8 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const Admins = require('./../models/Admin')
-<<<<<<< HEAD
-<<<<<<< HEAD
 const Lawyer = require('./../models/Lawyer')
 const Reviewer = require('./../models/Reviewer')
-=======
->>>>>>> master
 const validator = require('../../validations/AdminValidations')
 const Case = require('../models/Cases')
 const Lawyer = require('../models/Lawyer')
@@ -18,7 +14,6 @@ var nodemailer = require('nodemailer');
 var bcrypt = require('bcryptjs');
 
 
-=======
 const Case = require('./../models/Cases')
 const Lawyer = require('./../models/Lawyer')
 const Reviewer = require('./../models/Reviewer')
@@ -28,18 +23,11 @@ const validator = require('../../validations/AdminValidations')
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
->>>>>>> 6c9cf1eca2fddddd1f5055d3f9abdeac9c5a59c8
 
 var InvestorController = require('./InvestorController')
 
 let AdminController = {
-<<<<<<< HEAD
-//write your methods here: check investorController for example
-<<<<<<< HEAD
-=======
     //write your methods here: check investorController for example
-    //write your methods here: check investorController for example
->>>>>>> 6c9cf1eca2fddddd1f5055d3f9abdeac9c5a59c8
 
 
     /* Malak
@@ -247,133 +235,6 @@ let AdminController = {
     AdminRegisterAdmin: async (req, res) => {
         const AdminId = '5c9bb0dc5185793518ea84fb' //login token
         const Admin = await Admins.findById(AdminId)
-<<<<<<< HEAD
-=======
-system_assign_lawyer: async function (caseId) {
-    const Cases = await Case.findById(caseId)
-    const st = await Lawyer.find( { number_of_cases: 1 })
-    var least = st[0].number_of_cases
-    for (let i = 1; i < st.length; i += 1) {
-        if (st[i].number_of_cases < least) {
-            least = st[i].number_of_cases
-        }
-    }
-    for (let i = 0; i < st.length; i += 1) {
-        if (st[i].number_of_cases === least) {
-            fun.admin_assign_lawyer(caseId, st[i]._id)
-            break;
-        }
-    }
-},
-
-admin_assign_lawyer : async function (caseId, lawyerId) {
-    const updatedCase = await Case.findByIdAndUpdate(caseId, { 'lawyerID': lawyerId })
-    const st = await Lawyer.findById(lawyerId)
-    const updatedLawyer1 = await Lawyer.findByIdAndUpdate(lawyerId, { 'total_number_of_cases': st.total_number_of_cases + 1 })
-    const updatedLawyer2 = await Lawyer.findByIdAndUpdate(lawyerId, { 'number_of_cases': st.number_of_cases + 1 })
-},
-
-system_assign_reviewer : async function (caseId) {
-    const st = await Reviewer.find({ number_of_cases: 1 })
-    var least = st[0].number_of_cases
-    for (let i = 1; i < st.length; i += 1) {
-        if (st[i].number_of_cases < least) {
-            least = st[i].number_of_cases
-        }
-    }
-    for (let i = 0; i < st.length; i += 1) {
-        if (st[i].number_of_cases === least) {
-            fun.admin_assign_reviewer(caseId, st[i]._id)
-            break;
-        }
-    }
-},
-
-admin_assign_reviewer : async function (caseId, reviewerId) {
-    const updatedCase = await Case.findByIdAndUpdate(caseId, { 'reviewerID': reviewerId })
-    const st = await Reviewer.findById(reviewerId)
-    const updatedLawyer1 = await Reviewer.findByIdAndUpdate(reviewerId, { 'total_number_of_cases': st.total_number_of_cases + 1 })
-    const updatedLawyer2 = await Reviewer.findByIdAndUpdate(reviewerId, { 'number_of_cases': st.number_of_cases + 1 })
-
-},
-
-forgotpassword: async  (mail)=> {
-    //var userEmail = req.body.email;
-    var userEmail = mail
-    Admins.findOne({ email: userEmail }, function (err, user) {
-        if (err) {
-            res.json({ success: false, message: err.message });
-        }
-        else if (!user) {
-            res.json({ success: false, message: "incorrect email" });
-        }
-        else {
-
-            var token = jwt.sign({
-                _id: Admins._id,
-                FName: user.FName
-            }, 'secret', {expiresIn: 60 }); //seconds
-            
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'angry.nerds2019@gmail.com',
-                    pass: 'Angry1234'
-                }
- 
-            });
-            let mailOptions = {
-                from: '"Angry Nerds 👻" <angry.nerds2019@gmail.com>', // sender address
-                to: userEmail, // list of receivers
-                subject: 'Resetting Password', // Subject line
-                text: 'reset Link expires in 24 hours', // plain text body
-                html: '<h3>The code expires within an hour</h3> <br> <p>Click <a href="http://localhost:3000/resetpass/' + token + '">here</a> to reset your password</p>'
-                // html body
-            };
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    return console.log(error);
-                }
-                user.token = token;
-                user.save();
-                res.json({ success: true, message: 'An email has been sent check your email' });
-            });
-        }
-    });
- },
-
- resetpassword : function  (req, res) {
-            var userToken =  req.params.token;
-            var newPassword = req.body.password;
-            Admins.findOne({ token: userToken }, function (err, user) {
-                if (err) {
-                    res.json({ success: false, message: "Token is expired please try again" });
-                }
-                else {
-                    if ((Date.now()-user.token_date) > 60*60*1000){
-                        res.json({ success: false, message: "Token is expired please try again" });
-                    }
-                    else{
-                        bcrypt.genSalt(10, function (err, salt) {
-                            bcrypt.hash(newPassword, salt, function (err, hash) {
-                                user.password = hash;
-                                user.save(function (err) {
-                                    if (err) {
-                                        res.json({ success: false, message: err.message });
-                                        console.log(err);
-                                    }
-                                    else {
-                                        res.json({ success: true, message: "Password reseted succesfully" });
-                                    }
-                                });
-                            });
-                        });
-                    }
-                }
-            });
- }
->>>>>>> master
-=======
         if ((!Admin) || (Admin && Admin.Type !== 'Super'))
             return res.status(403).json({ error: 'Only syuper admins have access' })
         const email = req.body.email
@@ -515,20 +376,134 @@ forgotpassword: async  (mail)=> {
 
         }
     },
+    system_assign_lawyer: async function (caseId) {
+        const Cases = await Case.findById(caseId)
+        const st = await Lawyer.find({ number_of_cases: 1 })
+        var least = st[0].number_of_cases
+        for (let i = 1; i < st.length; i += 1) {
+            if (st[i].number_of_cases < least) {
+                least = st[i].number_of_cases
+            }
+        }
+        for (let i = 0; i < st.length; i += 1) {
+            if (st[i].number_of_cases === least) {
+                fun.admin_assign_lawyer(caseId, st[i]._id)
+                break;
+            }
+        }
+    },
 
+    admin_assign_lawyer: async function (caseId, lawyerId) {
+        const updatedCase = await Case.findByIdAndUpdate(caseId, { 'lawyerID': lawyerId })
+        const st = await Lawyer.findById(lawyerId)
+        const updatedLawyer1 = await Lawyer.findByIdAndUpdate(lawyerId, { 'total_number_of_cases': st.total_number_of_cases + 1 })
+        const updatedLawyer2 = await Lawyer.findByIdAndUpdate(lawyerId, { 'number_of_cases': st.number_of_cases + 1 })
+    },
 
->>>>>>> 6c9cf1eca2fddddd1f5055d3f9abdeac9c5a59c8
+    system_assign_reviewer: async function (caseId) {
+        const st = await Reviewer.find({ number_of_cases: 1 })
+        var least = st[0].number_of_cases
+        for (let i = 1; i < st.length; i += 1) {
+            if (st[i].number_of_cases < least) {
+                least = st[i].number_of_cases
+            }
+        }
+        for (let i = 0; i < st.length; i += 1) {
+            if (st[i].number_of_cases === least) {
+                fun.admin_assign_reviewer(caseId, st[i]._id)
+                break;
+            }
+        }
+    },
+
+    admin_assign_reviewer: async function (caseId, reviewerId) {
+        const updatedCase = await Case.findByIdAndUpdate(caseId, { 'reviewerID': reviewerId })
+        const st = await Reviewer.findById(reviewerId)
+        const updatedLawyer1 = await Reviewer.findByIdAndUpdate(reviewerId, { 'total_number_of_cases': st.total_number_of_cases + 1 })
+        const updatedLawyer2 = await Reviewer.findByIdAndUpdate(reviewerId, { 'number_of_cases': st.number_of_cases + 1 })
+
+    },
+
+    forgotpassword: async (mail) => {
+        //var userEmail = req.body.email;
+        var userEmail = mail
+        Admins.findOne({ email: userEmail }, function (err, user) {
+            if (err) {
+                res.json({ success: false, message: err.message });
+            }
+            else if (!user) {
+                res.json({ success: false, message: "incorrect email" });
+            }
+            else {
+
+                var token = jwt.sign({
+                    _id: Admins._id,
+                    FName: user.FName
+                }, 'secret', { expiresIn: 60 }); //seconds
+
+                let transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'angry.nerds2019@gmail.com',
+                        pass: 'Angry1234'
+                    }
+
+                });
+                let mailOptions = {
+                    from: '"Angry Nerds 👻" <angry.nerds2019@gmail.com>', // sender address
+                    to: userEmail, // list of receivers
+                    subject: 'Resetting Password', // Subject line
+                    text: 'reset Link expires in 24 hours', // plain text body
+                    html: '<h3>The code expires within an hour</h3> <br> <p>Click <a href="http://localhost:3000/resetpass/' + token + '">here</a> to reset your password</p>'
+                    // html body
+                };
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    user.token = token;
+                    user.save();
+                    res.json({ success: true, message: 'An email has been sent check your email' });
+                });
+            }
+        });
+    },
+
+    resetpassword: function (req, res) {
+        var userToken = req.params.token;
+        var newPassword = req.body.password;
+        Admins.findOne({ token: userToken }, function (err, user) {
+            if (err) {
+                res.json({ success: false, message: "Token is expired please try again" });
+            }
+            else {
+                if ((Date.now() - user.token_date) > 60 * 60 * 1000) {
+                    res.json({ success: false, message: "Token is expired please try again" });
+                }
+                else {
+                    bcrypt.genSalt(10, function (err, salt) {
+                        bcrypt.hash(newPassword, salt, function (err, hash) {
+                            user.password = hash;
+                            user.save(function (err) {
+                                if (err) {
+                                    res.json({ success: false, message: err.message });
+                                    console.log(err);
+                                }
+                                else {
+                                    res.json({ success: true, message: "Password reseted succesfully" });
+                                }
+                            });
+                        });
+                    });
+                }
+            }
+        });
+    }
+
 
 
 }
 
-<<<<<<< HEAD
-}
-<<<<<<< HEAD
-=======
->>>>>>> master
-module.exports = AdminController
-=======
+
 
 module.exports = AdminController
->>>>>>> 6c9cf1eca2fddddd1f5055d3f9abdeac9c5a59c8
