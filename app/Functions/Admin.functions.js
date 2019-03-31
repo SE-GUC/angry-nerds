e5b07b12ff7f9bb4f421871fa4a87b5877515f71
PaulAshraf <paulashraf14@gmail.com>
2019-03-31 21:58:14 +0200

    added test cases working

--- app/Functions/Admin.functions.js
+++ app/Functions/Admin.functions.js
@@ -7,7 +7,7 @@ const functions = {
     MailForgotPassword : async (mail) => {
         const t = await axios({
             method: 'post',
-            url:'http://localhost:3000/forgotpassword',
+            url:'http://127.0.0.1:3000/forgotpassword',
             headers: {}, 
             data: {
               email: mail, // This is the body part
@@ -15,6 +15,18 @@ const functions = {
           });
           return t.data.message
     },
+
+    adminChangePassword: async (id,old,newPass) =>{
+      const body = {
+          oldPassword: old,
+          newPassword: newPass
+      }
+    //  console.log('Im hererer')
+      const response = await axios.put('http://127.0.0.1:3000/AdminChangePassword/' + id , body)
+    //  console.log(response)
+
+      return response;
+  },
 
 }
 module.exports = functions;
--- test.js
+++ test.js
@@ -1,16 +1,126 @@
 const funcs = require('./fn');
 
-const f = require('./app/Functions/Admin.functions');
+const Admin = require('./app/Functions/Admin.functions');
+const Investor = require('./app/Functions/Investor.functions');
 
 test('Forgot password with valid mail', async () => {
-  const msg =  await f.MailForgotPassword('fady.wasfalla@gmail.com')
+  jest.setTimeout(30000)
+  const msg =  await Admin.MailForgotPassword('fady.wasfalla@gmail.com')
   expect(msg).toEqual('An email has been sent check your email');
 });
 
 test('Forgot password with invalid mail', async () => {
-  const msg =  await f.MailForgotPassword('Wrong_mail@gmail.com')
+  const msg =  await Admin.MailForgotPassword('Wrong_mail@gmail.com')
   expect(msg).toEqual('incorrect email');
 });
+
+
+test ('Updates Password Investor with valid ID and valid old password', async () => {
+  var validInvestorID = "5c7a9b46470a360ac8b0d412"
+  var validOldPassword = "newPass"
+  var newPassword = "newPass"
+  //expect.assertions(1)
+  const response = await Investor.investorChangePassword(validInvestorID,validOldPassword,newPassword)
+  expect(response.data.msg).toEqual('The password was updated')
+})
+
+
+test ('Updates Password Investor with valid ID and invalid old password', async () => {
+  var validInvestorID = "5c7a9b46470a360ac8b0d412"
+  var validOldPassword = "x"
+  var newPassword = "newPass"
+  //expect.assertions(1)
+
+  try {
+    const response = await Investor.investorChangePassword(validInvestorID,validOldPassword,newPassword)
+  } catch (e) {
+    //console.log(e.response.data)
+    expect(e.response.data.error).toMatch('The passwords do not match');
+  }
+  
+})
+
+test ('Updates Password Investor with invalid ID and valid old password', async () => {
+  var validInvestorID = "x"
+  var validOldPassword = "newPass"
+  var newPassword = "newPass"
+  //expect.assertions(1)
+
+  try {
+    const response = await Investor.investorChangePassword(validInvestorID,validOldPassword,newPassword)
+  } catch (e) {
+   // console.log(e.response.data)
+    expect(e.response.data.error).toMatch('Error processing query.');
+  }
+  
+})
+
+test ('View notification of an investor', async () => {
+  var validInvestorID = "5c7a9b46470a360ac8b0d412"
+  //expect.assertions(1)
+    const response = await Investor.investorMyNotifications(validInvestorID)
+    //console.log(response)
+    expect(response.data.msg).toEqual('Done')
+
+  
+})
+
+test ('View notification of an investor with an invalid ID', async () => {
+  var validInvestorID = "x"
+  //expect.assertions(1)
+  try {
+    const response = await Investor.investorMyNotifications(validInvestorID)
+  } catch (e) {
+   // console.log(e.response.data)
+    expect(e.response.data.error).toMatch('Error processing query.');
+  }
+  
+})
+
+test ('View pending companies of an investor', async () => {
+  var validInvestorID = "5c7a9b46470a360ac8b0d412"
+  //expect.assertions(1)
+    const response = await Investor.viewMyPendingCompanies(validInvestorID)
+    //console.log(response)
+    expect(response.data.msg).toEqual('Done')
+
+  
+})
+
+test ('View pending companies of an investor with an invalid ID', async () => {
+  var validInvestorID = "x"
+  //expect.assertions(1)
+  try {
+    const response = await Investor.viewMyPendingCompanies(validInvestorID)
+  } catch (e) {
+    //console.log(e.response.data)
+    expect(e.response.data.error).toMatch('Error processing query.');
+  }
+  
+})
+
+test ('generate a PDF with a valid ID', async () => {
+  var validCaseID = "5c9cfd1d05f1d42e68b75fb7"
+  //expect.assertions(1)
+    const response = await Investor.generatePdf(validCaseID)
+    //console.log(response)
+    expect(response.data.msg).toEqual('Done')
+
+  
+})
+
+test ('generate a PDF with a invalid ID', async () => {
+  jest.setTimeout(30000)
+  var validCaseID = "x"
+  //expect.assertions(1)
+  try {
+    const response = await Investor.generatePdf(validCaseID)
+  } catch (e) {
+    //console.log(e.response.data)
+    expect(e.response.data.error).toMatch('Error processing query.');
+  }
+  
+})
 
 
 
