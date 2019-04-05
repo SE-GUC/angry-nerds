f0b1d9907cd1146f9c3bd97c89cf629877f253d1
RamyGabra <RamyGabra5@gmail.com>
2019-04-05 18:40:32 +0200

    restructuring

--- app/Tests/Reviewer.tests.js
+++
@@ -1,3 +0,0 @@
-const functions = require ('../app/Functions/Reviewer.functions')
-
-test ()
--- config/mailer.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: 'angry.nerds2019@gmail.com',
-    pass: 'Angry1234'
-    //MAILGUN_USER: 'postmaster@sandbox7e0952227a0744be86d9c31845b46c94.mailgun.org',
-    //MAILGUN_PASS: '2f270a3a4c00f76af1342b36fbb0cf92-e51d0a44-cefb7270'
-}
