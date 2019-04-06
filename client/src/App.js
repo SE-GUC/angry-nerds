ee2a348794dac56df201580b3421dec987392048
RamyGabra <RamyGabra5@gmail.com>
2019-04-06 15:34:17 +0200

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Payment from './pages/payment'
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+
+  //states & functions
+  state = {
+    case: {
+      _id: '5c95094155f85f30d82dcfeb',
+      form_type: 'SSCP',
+      regulated_law: 'Law 159',
+      arabic_name: 'تتتت',
+      english_name: 'Hello6',
+      government: "ENG",
+      city: 'Cairo',
+      hq_address: 'gftfy',
+      hq_city: 'yes',
+      main_center_phone: 123515,
+      main_center_fax: 518563,
+      currency: '541',
+      fees:532,
+      equality_capital: 505464562,
+      caseStatus: 'pending'
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <h1>Hello World!</h1>
+              <h2> my name is Ramy! </h2>
+            </React.Fragment>
+          )} />
+
+          <Route exact path="/about" render={props => (
+            <React.Fragment>
+              <h1>Hello World!</h1>
+              <h2> my name is romba! </h2>
+            </React.Fragment>
+          )} />
+
+          <Route exact path="/payment" render={props => (
+            <React.Fragment>
+              <Payment case={this.state.case} />
+            </React.Fragment>
+          )} />
+
+
+
+        </div>
+      </Router>
     );
   }
 }
