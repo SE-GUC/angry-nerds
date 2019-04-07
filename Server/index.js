691e0ba4fca2642060131ad21fb71f69327ff82b
nadeengogary <nadeengogary@gmail.com>
2019-04-07 20:10:36 +0200

    nadeen react

--- Server/index.js
+++ Server/index.js
@@ -341,6 +341,11 @@ app.get('/image/:filename', (req, res) => {
 app.get('/chat', function (req, res) {
   res.sendFile(__dirname + '/views/chat.html');
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/Staff', Staffi)
 // app.use('/api/Cases', Cases)
 app.use('/api/Investor', investor)
@@ -351,8 +356,6 @@ app.use('/api/Admin', Admin)
 app.use('/', routes)
 
 app.use((req, res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
-
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server on ${port}`))
