a6e34687d62dc1cca465cbbb68cb6fcf8cdc4672
fadyHemaya <fady.hemaya@student.guc.edu.eg>
2019-04-07 19:03:38 +0200

    sign in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
+import signup from './pages/signup'
+import signin from './pages/signin'
+import home from './pages/home'
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+          <Route  exact path = "/home" component = {home}  />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/signin" component= {signin} />
+          </div>  
+        </div>
+      </Router>
     );
   }
+  
 }
 
 export default App;
