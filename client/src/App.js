32f56570a3ee4d7bbcbf6f527e6ca69baac13cd3
DanielAshrafHanna <danielhanna0001@gmail.com>
2019-04-14 04:45:34 +0200

    done with user stories

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Payment from './pages/payment'
 import Notification from './components/notification'
-import './App.css';
+// import './App.css';
 import LawyerForm from './pages/LawyerForm'
 import InvestorForm from './pages/InvestorForm'
 import axios from 'axios'
 import SideNav from './layout/sideNav'
 import TrackMyCase from './pages/trackMyCase'
 import Companies from './pages/Companies';
-import signup from './pages/signUp'
+// import signup from './pages/signUp'
 import signin from './pages/signin'
 import home from './pages/home'
 import LawyerHome from './pages/LawyerHome'
 import ChangePricing from './pages/ChangePricing'
 import AdminViewLaws from './pages/AdminViewLaws'
 import AddLawyer from './components/AddLawyer';
+import AdminAddLawyer from './components/AdminAddLawyer';
+import AdminAddReviewer from './components/AdminAddReviewer';
+import AdminAddAdmin from './components/AdminAddAdmin';
 import AddReviewer from './components/AddReviewer';
 import testing from './components/testing';
+import test from './components/test';
+
 
 
 
@@ -26,41 +31,7 @@ class App extends Component {
 
   
   //states & functions
-  state = {
-      notifications: [],
-      case:
-      {
-        _id: "5c95094155f85f30d82dcfeb",
-        form_type: "SSCP",
-        regulated_law: "masr",
-        arabic_name: "تتتت",
-        english_name: "Hello6",
-        government: "ENG",
-        city: "Cairo",
-        hq_address: "gftfy",
-        hq_city: "yes",
-        main_center_phone: 123515,
-        main_center_fax: 518563,
-        currency: "541",
-        equality_capital: 5054641641562,
-        caseStatus: "published",
-        investorID: "5ca772654d70710fa843bd5f",
-    }
-  }
-
-  componentDidMount() {
-    setInterval(() => axios.get('http://localhost:3000/investorMyNotifications/5ca772654d70710fa843bd5f')
-    .then(res =>{
-      this.setState({ notifications: res.data.data })
-    } ), 1000)
-    
-      
-  }
-
  
-   componentWillUnmount() {
-     clearInterval(this.state)
-   }
 
 
   render() {
@@ -101,14 +72,21 @@ class App extends Component {
 
           <Route exact path = '/ViewMyCompanies' component =  {Companies}/>
           <Route  exact path = "/home" component = {home}  />
-            <Route exact path="/signUp" component={signup} />
-            <Route exact path="/signin" component= {signin} />
-            <Route exact path="/AddLawyer" component={AddLawyer} />
+          {/* <Route exact path="/signUp" component={signup} /> */}
+          <Route exact path="/signin" component= {signin} />
+          <Route exact path="/AddLawyer" component={AddLawyer} />
           <Route exact path="/AddReviewer" component={AddReviewer} /> 
           <Route  exact path="/testing" component={testing} />
           <Route exact path = "/LawyerHome" component={LawyerHome} />
-        <Route exact path = "/ChangePricing" component={ChangePricing} />
-        <Route exact path = "/AdminViewLaws" component={AdminViewLaws} /> 
+          <Route exact path = "/ChangePricing" component={ChangePricing} />
+          <Route exact path = "/AdminViewLaws" component={AdminViewLaws} /> 
+          <Route exact path = "/AdminAddLawyer" component={AdminAddLawyer} /> 
+          <Route exact path = "/AdminAddReviewer" component={AdminAddReviewer} /> 
+          <Route exact path = "/AdminAddAdmin" component={AdminAddAdmin} /> 
+
+
+
+
 
 
           <Route exact path="/notification" render={props => (
@@ -122,6 +100,8 @@ class App extends Component {
         
         
         </div>
+        <Route exact path = "/test" component={test} /> 
+
       </Router>
      );
    }
---
+++ client/src/components/AdminAddAdmin.js
@@ -0,0 +1,213 @@
+import React, { Component } from 'react'
+import {Button,Form,Col,Modal} from 'react-bootstrap';
+import './AddLawyer.css'
+import axios from 'axios';
+
+export class AdminAddAdmin extends Component {
+    setstate = {
+        FName: '',
+        MName: '',
+        LName: '',
+        email: '',
+        password: '',
+        gender: '',
+        Nationality: '',
+        birthdate: '',
+        Address: '',
+        telephone_number: '',
+        completed_number_of_cases :'0',
+        number_of_cases: '0' ,
+        total_time_on_cases: '0' ,
+        fax: '',
+        ssid:''
+      }
+    
+      handleChangeFName = event => {
+        this.setState({ FName: event.target.value});                                
+      }
+    
+      handleChangeMName = event => {
+        this.setState({ MName: event.target.value});                                
+      }
+      handleChangeLName = event => {
+        this.setState({ LName: event.target.value});                                
+      }
+    
+      handleChangeEmail = event => {
+        this.setState({ email: event.target.value});                                
+      }
+      handleChangePassword = event => {
+        this.setState({ password: event.target.value});                                
+      }
+    
+      handleChangeGender = event => {
+        this.setState({ gender: event.target.value});                                
+      }
+      handleChangrNationality = event => {
+        this.setState({ Nationality: event.target.value});                                
+      }
+      handleChangebirthdate = event => {
+        this.setState({ birthdate: event.target.value});                                
+      }
+    
+      handleChangegAddress = event => {
+        this.setState({ Address: event.target.value});                                
+      }
+      handleChangegtelephone_number = event => {
+        this.setState({ telephone_number: event.target.value});                                
+      }
+    
+      handleChangegfax = event => {
+        this.setState({ fax: event.target.value});                                
+      }
+    
+      handleChangegssid = event => {
+        this.setState({ ssid: event.target.value});                                
+      }
+    
+      
+    
+      handleSubmit = event => {
+        event.preventDefault();
+    
+        const user = {
+          FName: this.state.FName,
+          MName: this.state.MName,
+          LName: this.state.LName,
+          email: this.state.email,
+          password: this.state.password,
+          gender: this.state.gender,
+          Nationality: this.state.Nationality,
+          birthdate: this.state.birthdate,
+          Address: this.state.Address,
+          telephone_number: this.state.telephone_number,
+          fax: this.state.fax,
+          completed_number_of_cases: '0',
+          total_time_on_cases: '0',
+          number_of_cases: '0' ,
+          ssid: this.state.ssid,
+    
+    
+    
+         };
+    
+         try {
+        axios.post(`http://localhost:3000/AdminRegisterAdmin`,  user )
+          .then(res => {
+            console.log(res);
+            console.log(res.data);
+          }) }
+          catch(e){
+            console.log(e)
+          }
+    
+      }
+      
+      render() {
+        return (
+          <Form onSubmit={this.handleSubmit}>
+
+          <Modal.Title>ADD AN ADMIN</Modal.Title>
+
+
+          
+
+
+    
+          <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" name="email" placeholder="Enter email"  onChange={this.handleChangeEmail}/>
+        </Form.Group>
+    
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="text"  name="password" placeholder="Password"  onChange={this.handleChangePassword}/>
+        </Form.Group>
+      </Form.Row>
+    
+           <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text"  name="FName"   placeholder="Enter First Name"  onChange={this.handleChangeFName}/>
+        </Form.Group>
+    
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Middle Name</Form.Label>
+          <Form.Control type="text"  name="MName" placeholder="Enter Middle Name"  onChange={this.handleChangeMName}/>
+        </Form.Group>
+    
+           <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" name="LName" placeholder="Enter Last Name"  onChange={this.handleChangeLName}/>
+        </Form.Group>
+    
+      </Form.Row>
+    
+      <Form.Row> 
+    
+      <Form.Group as ={Col} controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control  name="Address"  onChange={this.handleChangegAddress} />
+      </Form.Group>
+    
+      <Form.Group as={Col} controlId="formGridAddress1">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control name="telephone_number" onChange={this.handleChangegtelephone_number} />
+      </Form.Group>
+      </Form.Row>
+    
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>Nationality</Form.Label>
+          <Form.Control name="Nationality" onChange={this.handleChangrNationality} />
+        </Form.Group>
+    
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Gender</Form.Label>
+          <Form.Control as="select"  name="Gender" onChange={this.handleChangeGender}   >
+            <option>...</option>
+            <option>Female</option>
+            <option>Male</option>
+    
+          </Form.Control>
+        </Form.Group>
+    
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>BithDate</Form.Label>
+          <Form.Control type = "date"  name="BithDate" onChange={this.handleChangebirthdate}/>
+        </Form.Group>
+      </Form.Row>
+    
+         <Form.Row>
+    
+    <Form.Group as={Col} controlId="formGridZip">
+    <Form.Label>fax</Form.Label>
+    <Form.Control type = "text"  name="fax" onChange={this.handleChangegfax}/>
+    </Form.Group>
+    
+    <Form.Group as ={Col} controlId="formGridZip">
+    <Form.Label>ssid</Form.Label>
+    <Form.Control type = "text"  name="fax" onChange={this.handleChangegssid}/>
+    </Form.Group>
+    
+    </Form.Row>
+
+
+    
+    
+    
+    
+    
+    <Button variant="outline-secondary"  type="submit"  >Submit</Button>
+
+
+      
+          </Form>
+
+          
+        );
+      }
+      
+    }
+export default AdminAddAdmin
