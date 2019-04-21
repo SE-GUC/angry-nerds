4fbaa3589cc59b02266cc4c2d41be94672342e3d
fady-wasfalla <fady.wasfalla@gmail.com>
2019-04-21 13:37:51 +0200

    Sign in new Style

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'; 
 import jwt from 'jsonwebtoken'
 import axios from 'axios';
-import { Row } from 'react-bootstrap';
+import {InputGroup,FormControl,Row,Col} from "react-bootstrap";
 import '../App.css'
 import { Route, Redirect } from 'react-router-dom'
 import setAuthToken from '../helpers/setAuthToken';
@@ -12,22 +12,23 @@ const image1 =require('../Images/logo.png')
 
 class signin extends Component  {
   state={
-    toHome : false
+    toHome : false,
+    type: 'password',
+    email:'',
+    password:''
   }
     async submit (event){
       event.preventDefault();
-      var semail = document.getElementById("email").value
-      var spassword = document.getElementById("password").value
-      console.log(semail)
-      console.log(spassword)
+      console.log(this.state.email)
+      console.log(this.state.password)
       try{
 
         const user = await axios({
             method: "post",
             url: "http://localhost:3000/login" ,
             data: {
-              email : semail,
-              password : spassword
+              email : this.state.email,
+              password : this.state.password
             }
           })
           console.log('HAHAHAHAHAHA',user.data)
@@ -51,60 +52,45 @@ class signin extends Component  {
           alert(error)
         }
     }
+
+    handleClick = () => this.setState(({type}) => ({
+      type: type === 'text' ? 'password' : 'text'
+    }))
+
     render(){
       if (this.state.toHome===true){
         return (<Redirect to={{pathname:'/LawyerHome'}} />)
         }else{
     return (
-        <React.Fragment>
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-<Form>
-<div>
-  <div style={{textAlign:"center"}}>
-<img src={image1} class="rounded"/>
-</div>
-</div>
-<br>
-</br>
-<fieldset class="the-fieldset">
-   <legend  class="the-legend" >Sign in</legend>
-<div className="container">
-<Row >
-<div className="col-xs-4">
-<Row>
-<h4>  <label className="label label-primary" for="ex2">Email</label> </h4>
-</Row>
-  <Form.Group controlId="formBasicEmail">
-    <input class="form-control" type="email" placeholder="Enter email"  id="email"/>
-  </Form.Group>
-  </div>
-  </Row>
-  <Row>
-  <h4><label className="label label-primary" for="ex2">Password</label></h4>
-  </Row>
-  <Row>
-  <div className="col-xs-4">
-  <Form.Group controlId="formBasicPassword">
-    <input class="form-control" type="password" placeholder="Enter password"  id="password"/>
-
- 
-  </Form.Group>
-  </div>
-  </Row>
-  <br></br>
-  <Row>
-  <Button variant="primary" type="submit" onClick={this.submit.bind(this)}>
-    Sign in
-  </Button>
-  </Row>
-  </div>
-  </fieldset>
-</Form>
-</React.Fragment>        
+      <React.Fragment>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <Row style={{height: .12*window.innerHeight + 'px'}}>  </Row>
+        <legend  class="the-legend" style={{color: "#428bca"}}>Sign in</legend>        
+        <Col md={{ span: 2, offset: 0 }}>
+        <Row style={{height: .04*window.innerHeight + 'px'}} />
+         <img src={image1} /> </Col>
+        <Col md={{ span: 4, offset: 2 }}> 
+        <Row style={{height: .05*window.innerHeight + 'px'}} />
+        <InputGroup className="mb-3">
+        <Form.Label style={{color: "#428bca"}}>Email address</Form.Label>
+        <FormControl type="mail" placeholder="mail@example.com" 
+        onChange={(e) => {this.setState({email:e.target.value})}} required/>
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <Form.Label style={{color: "#428bca"}}>Password</Form.Label>
+        <FormControl type={this.state.type} placeholder="Password" 
+        onChange={(e) => {this.setState({password:e.target.value})}} required />
+        <InputGroup.Append>
+          <Button className="glyphicon glyphicon-eye-open" variant="outline" onClick={this.handleClick} />
+        </InputGroup.Append>
+      </InputGroup>
+      <Row style={{height: .03*window.innerHeight + 'px'}}>  </Row>
+      <Col md={{ span: 4, offset: 8 }}>
+      <Button variant="primary" type="submit" onClick={this.submit.bind(this)}>Sign in</Button></Col>
+      </Col>
+      </React.Fragment>        
   )
   
     }
